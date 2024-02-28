0f23527f6c388f5fe752d02a4ea908fae0afc480
董玉岭 <13374034+dong-yuling@user.noreply.gitee.com>
2024-02-28 15:04:09 +0800

    two

--- grpc/grpc.go
+++ grpc/grpc.go
@@ -7,17 +7,26 @@ import (
 	"net"
 
 	grpc2 "google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/reflection"
 )
 
-func Register(port, register func(s *grpc2.Server)) error {
+func Register(port int, register func(s *grpc2.Server), cert, key string) error {
 
 	flag.Parse()
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc2.NewServer()
+
+	// Create tls based credential.
+	creds, err := credentials.NewServerTLSFromFile(cert, key)
+	if err != nil {
+		log.Fatalf("failed to create credentials: %v", err)
+	}
+
+	s := grpc2.NewServer(grpc2.Creds(creds))
+	// 反射查询
 	reflection.Register(s)
 	register(s)
 	log.Printf("server listening at %v", lis.Addr())
